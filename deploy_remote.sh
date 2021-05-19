9f506a321fa0d8e77910d6f4605b571b1575cf6c
Jdar15 <83215247+Jdar15@users.noreply.github.com>
2021-05-19 15:46:44 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -8,8 +8,11 @@ PATH_SECRET_KEY=/secrets/kp-julie-1.pem
 # On installe java sur la machine distante
 ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
 
-
+#On crée le dossier /app sur une machine distante
 ssh  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec2-user /app'
+#On copie le fichier .class dans le dossier homedir de l'utilisateur
 scp  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
+#On copie le fichier.class dans le dossier /app localement
 ssh  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
+# On execute le fichier helloWorld.clas
 ssh  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
