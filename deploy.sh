a5d1ea19a0a791d608386dc42cbaf82233f82673
Jdar15 <83215247+Jdar15@users.noreply.github.com>
2021-05-19 11:00:29 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 # deploy.sh
-sudo mkdir -p /test1
-sudo cp helloWorld.class /test1
-sudo cd /test1
+pwd
+mkdir -p test1/
+sudo cp helloWorld.class test1
+sudo cd test1
 pwd
 java helloWorld
