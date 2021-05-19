e1286651233bab68f910bf5c42cb5dcdd6da4845
Jdar15 <83215247+Jdar15@users.noreply.github.com>
2021-05-19 15:18:04 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -3,7 +3,7 @@
 
 ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com 'sudo java-version'
 ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com <<EOF
-if [[#? != 0]] 
+if [[ $? != 0]] 
   then sudo amazon-linux-extras install -y java-openjdk11'
 fi
 EOF
