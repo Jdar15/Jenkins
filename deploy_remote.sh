6517ebe1fb7000115f6867fdcb1c93c2422b3b52
Jdar15 <83215247+Jdar15@users.noreply.github.com>
2021-05-19 15:40:16 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 # deploy_remote.sh
 
-ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com 'sudo amazon-linux-extras install -y java-openjdk11'
+# Je mets la chaine de connexion de ma machine distante dans une variable
+MACHINE_DESTINATION=ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com
+PATH_SECRET_KEY=/secrets/kp-julie-1.pem
 
-ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com 'sudo mkdir /app && sudo chmod -R 666 /app'
-scp -i /secrets/kp-julie-1.pem helloWorld.class ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com:/home/ec2-user/helloWorld.class
-ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com  'sudo mv /home/ec2-user/helloWorld.class /app'
-ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com  'sudo cd /app && sudo java helloWorld'
+# On installe java sur la machine distante
+ssh -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo amazon-linux-extras install -y java-openjdk11'
+
+
+ssh  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo mkdir -p /app && sudo chmod -R 777 /app && sudo chown ec'
+scp  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY helloWorld.class $MACHINE_DESTINATION:/home/ec2-user/helloWorld.class
+ssh  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cp /home/ec2-user/helloWorld.class /app'
+ssh  -o StrictHostKeyChecking=no -i $PATH_SECRET_KEY $MACHINE_DESTINATION 'sudo cd /app && sudo java helloWorld'
