bc887ffeaa4687072e4dde4f52916c900a6a42a2
Jdar15 <83215247+Jdar15@users.noreply.github.com>
2021-05-19 15:12:34 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -2,7 +2,11 @@
 # deploy_remote.sh
 
 ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com 'sudo java-version'
-ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com 'if [[#? != 0]] then sudo amazon-linux-extras install -y java-openjdk11'
+ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com <<EOF
+if [[#? != 0]] 
+  then sudo amazon-linux-extras install -y java-openjdk11'
+fi
+EOF
 ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com 'sudo mkdir /app'
 scp -i /secrets/kp-julie-1.pem helloWorld.class ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com:/app
 ssh -i /secrets/kp-julie-1.pem ec2-user@ec2-3-14-133-129.us-east-2.compute.amazonaws.com  'sudo cd /app && sudo java helloWorld'
